4343f2995ca07dcc26ca3f268617573951491a93
Ritesh <riteshpatel246@gmail.com>
2023-07-27 22:11:00 +0530

    some update

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 
-import blog from './images/blog.png';
-const App =() => {
-    return (
-        <Container maxidth="lg">
-                <AppBar position="static" color="inherit">
-                    <Typography variant="h2" align="center">Blogger</Typography>
-                    <img src={blog} alt="memories" height="300" /> 
-                </AppBar>
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import { getPosts } from './actions/posts';
+import useStyles from './styles';
+// import memories from './images/blog.png';
+
+const App = () => {
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h3" align="center">MAKE YOUR MEMORIES :)</Typography>
+        {/* <img className={classes.image} src={memories} alt="icon" height="60" /> */}
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
         </Container>
-    );
-}
+      </Grow>
+    </Container>
+  );
+};
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -7,19 +7,19 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://JavaScript:JavaScript123@cluster0.clmaawl.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
-    // mongoose.set('useFindAndModify', false);  not working
+    // mongoose.set('useFindAndModify', false);
     // http://www.mongodb.com/cloud/atlas
     
